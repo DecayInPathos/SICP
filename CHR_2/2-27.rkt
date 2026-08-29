8ee5103943e6073efce57917f9fdf08a10c23786
DeacyInPathos <a13140471281@gmail.com>
2026-08-29 21:17:07 +0800

    0829

--- CHR_2/2-27.rkt
+++ CHR_2/2-27.rkt
@@ -1,3 +1,6 @@
 #lang sicp
-(define (deep-reverse x)
-  ())
+(define (deep-reverse list1)
+  (cond ((null? list1)nil)
+    ((not(pair? (car list1))) (append  (deep-reverse(cdr list1)) (list (car list1)) ) )
+    (else (append (deep-reverse(cdr list1)) (list (deep-reverse (car list1)))))))
+
