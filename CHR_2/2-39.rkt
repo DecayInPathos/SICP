67b24bc99e808b2064cbb46c7c783f4cacf99793
DeacyInPathos <a13140471281@gmail.com>
2026-09-02 09:48:09 +0800

    0902

---
+++ CHR_2/2-39.rkt
@@ -0,0 +1,22 @@
+#lang sicp
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+
+(define (fold-right op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (fold-right op initial (cdr sequence)))))
+(define (reverse0 sequence)
+  (fold-right (lambda (x y) (append y (list x))) nil sequence))
+
+(define (reverse00 sequence)
+  (fold-left (lambda (x y) (cons y x)) nil sequence))
+(reverse0 (list 1 2 3 4))
+(reverse00 (list 1 2 3 4))
